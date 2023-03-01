3a2eeabe300eebdf64cfd47a9222534e41adf3f4
chanleeip <chanleeip@gmail.com>
2023-03-01 10:18:17 +0530

    commit

--- .history/App_20230301003708.js
+++ .history/App_20230301003708.js
@@ -1,13 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from "./components/Header";
-import Add_page from "./screens/add_page";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Header />
-        <Add_page />
       <StatusBar style="auto" />
     </View>
   );
---
+++ screens/add_page.js
@@ -0,0 +1,50 @@
+import {Dimensions, StyleSheet, Text, View,Pressable} from 'react-native'
+import React from 'react'
+import Icon from 'react-native-vector-icons/Ionicons'
+
+const add_page   = () => {
+    return (
+        <View style={styles.container}>
+            <Pressable>
+            <View style={styles.button}>
+                <Text style={styles.heading}>All pages</Text>
+                <Icon style={styles.icon} name="add-outline"/>
+                    </View>
+            </Pressable>
+        <Pressable>
+            <View style={styles.button}>
+                        <Text style={styles.heading}>Add Page</Text>
+                <Icon style={styles.icon} name="arrow-forward"/>
+                    </View>
+        </Pressable>
+            </View>
+    )
+}
+
+export default add_page
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        alignItems:"center",
+        justifyContent:"space-evenly",
+        marginTop:40,
+        marginBottom:130
+    },
+    button:{
+      width:Dimensions.get("window").width-100,
+        backgroundColor:"#f5f5f5",
+        justifyContent:"center",
+        alignItems:"center",
+        height:300,
+        marginVertical:50
+    },
+    icon:{
+        fontSize:"50%"
+
+    },
+    heading:{
+        fontSize:"50%"
+    }
+
+})
